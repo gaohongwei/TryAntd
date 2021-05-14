49ad66044114ba266ec262e8635a9437b7d6785e
Gaohong Wei <gaohong@pm61data.com>
2021-05-13 22:45:25 -0700

    ProCard

--- config/routes_description.js
+++ config/routes_description.js
@@ -3,9 +3,9 @@ const ProItem = {
   name: "ProItem",
   icon: "smile",
   routes: [
-    { name: "Layout", path: "./layout", component: "./ProDescription/Layout" },
-    { name: "Item", path: "./item", component: "./ProDescription/item" },
-    { name: "DataSource", path: "./datasource", component: "./ProDescription/datasource" },
+    // { name: "Layout", path: "./layout", component: "./ProDescription/Layout" },
+    // { name: "Item", path: "./item", component: "./ProDescription/item" },
+    // { name: "DataSource", path: "./datasource", component: "./ProDescription/datasource" },
   ],
 };
 
--- src/pages/ProCard/layout.tsx
+++ src/pages/ProCard/layout.tsx
@@ -3,17 +3,29 @@ import ProCard from "@ant-design/pro-card";
 
 export default () => {
   return (
-    <ProCard>
-      <ProCard title="默认尺寸" tooltip="这是提示" style={{ width: 300 }}>
-        <div>Card content</div>
-        <div>Card content</div>
-        <div>Card content</div>
+    <>
+      <ProCard split="vertical" direction="column">
+        <ProCard title="默认尺寸" tooltip="这是提示" colSpan="30%">
+          <div>Card content</div>
+          <div>Card content</div>
+          <div>Card content</div>
+        </ProCard>
+        <ProCard title="小尺寸卡片" tooltip="这是提示" colSpan="30%">
+          <div>Card content</div>
+          <div>Card content</div>
+          <div>Card content</div>
+        </ProCard>
+        <ProCard title="默认尺寸" tooltip="这是提示" colSpan="30%">
+          <div>Card content</div>
+          <div>Card content</div>
+          <div>Card content</div>
+        </ProCard>
+        <ProCard title="默认尺寸" tooltip="这是提示" colSpan="30%">
+          <div>Card content</div>
+          <div>Card content</div>
+          <div>Card content</div>
+        </ProCard>
       </ProCard>
-      <ProCard title="小尺寸卡片" tooltip="这是提示" style={{ width: 300 }} size="small">
-        <div>Card content</div>
-        <div>Card content</div>
-        <div>Card content</div>
-      </ProCard>
-    </ProCard>
+    </>
   );
 };
